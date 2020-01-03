8de758e2967675b848f8cfcf1ef36af7829d77cc
Ho Man Chan <homan98@gmail.com>
2020-01-03 00:50:39 -0500

    Feature/update hapi server (#10)
    * Update to latest dependencies
    Update index server to support latest Hapi dependencies using promises
    Update plugin txt to remove empty new line
    Update yarn lock with latest dependencies
    * Fix handler response with promises

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const Hapi = require('hapi');
+const Hapi = require('@hapi/hapi');
 const JFile = require('jfile');
 const async = require('async');
 const filter = require('lodash').filter;
 const fetchPluginData = require('./fetch-plugin-data');
 
-const server = new Hapi.Server();
-server.connection({
+const server = new Hapi.Server({
     host: 'localhost',
     port: 3000
 });
@@ -13,32 +12,35 @@ server.connection({
 server.route({
     method: 'GET',
     path:'/',
-    handler: function (request, reply) {
-        return reply.redirect('https://github.com/pressbooks/pressbooks-recommended-plugins-server/');
+    handler: function (request, h) {
+        return h.redirect('https://github.com/pressbooks/pressbooks-recommended-plugins-server/');
     }
 });
 
 server.route({
     method: 'GET',
     path:'/api/plugin-recommendations',
-    handler: function (request, reply) {
+    handler: function (request, h) {
         const pluginsFile = new JFile( __dirname + '/plugins.txt' );
         const pluginsToFetch = filter( pluginsFile.lines );
-        return async.map(
-            pluginsToFetch,
-            fetchPluginData,
-            ( err, results ) => {
-                const plugins = filter(results);
-                return reply({
-                    info: {
-                        page: 1,
-                        pages: 1,
-                        results: plugins.length,
-                    },
-                    plugins
-                });
-            }
-        );
+        const promise = new Promise((resolve, reject) => {
+            async.map(
+                pluginsToFetch,
+                fetchPluginData,
+                ( err, results ) => {
+                    const plugins = filter(results);
+                    resolve({
+                            info: {
+                                page: 1,
+                                pages: 1,
+                                results: plugins.length,
+                            },
+                            plugins
+                    });
+                }
+            );
+        });
+        return promise;
     }
 });
 
