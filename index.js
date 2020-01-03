3e34e99000eca386cecda8a4a32101ed890e0059
Ho Man Chan <homan98@gmail.com>
2020-01-03 00:07:59 -0500

    Feature/update hapi server (#8)
    Update to latest dependencies
    Update index server to support latest Hapi dependencies using promises
    Update plugin txt to remove empty new line
    Update yarn lock with latest dependencies

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const Hapi = require('hapi');
+const Hapi = require('@hapi/hapi');
 const JFile = require('jfile');
 const async = require('async');
 const filter = require('lodash').filter;
 const fetchPluginData = require('./fetch-plugin-data');
 
-const server = new Hapi.Server();
-server.connection({
+const server = new Hapi.Server({
     host: 'localhost',
     port: 3000
 });
@@ -13,30 +12,30 @@ server.connection({
 server.route({
     method: 'GET',
     path:'/',
-    handler: function (request, reply) {
-        return reply.redirect('https://github.com/pressbooks/pressbooks-recommended-plugins-server/');
+    handler: function (request, h) {
+        return h.redirect('https://github.com/pressbooks/pressbooks-recommended-plugins-server/');
     }
 });
 
 server.route({
     method: 'GET',
     path:'/api/plugin-recommendations',
-    handler: function (request, reply) {
+    handler: async function (request, h) {
         const pluginsFile = new JFile( __dirname + '/plugins.txt' );
         const pluginsToFetch = filter( pluginsFile.lines );
-        return async.map(
+        return await async.map(
             pluginsToFetch,
             fetchPluginData,
             ( err, results ) => {
                 const plugins = filter(results);
-                return reply({
-                    info: {
-                        page: 1,
-                        pages: 1,
-                        results: plugins.length,
-                    },
-                    plugins
-                });
+                return {
+                        info: {
+                            page: 1,
+                            pages: 1,
+                            results: plugins.length,
+                        },
+                        plugins
+                };
             }
         );
     }
